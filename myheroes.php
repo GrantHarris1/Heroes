0b1614f87019030271e9c3a15d91e21489198d37
grantharriselectric <grantharriselectric@gmail.com>
2021-11-05 09:36:19 -0400

    works

--- myheroes.php
+++ myheroes.php
@@ -1,9 +1,9 @@
 <!-- * connect to database
 * switch for a route
 * run a crud function
-    * inside crude function check for errors
-    * access database for the crude function RUN A MYSQL QUERRY
-    * inside function return result of querry
+* inside crude function check for errors
+* access database for the crude function RUN A MYSQL QUERRY
+* inside function return result of querry
 * echo data to api of postman
 * close connection -->
 <?php
@@ -19,21 +19,13 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
+
+
+
+// CREATE
 function createHero($name, $about_me, $biography)
 {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Heroes";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    global $conn;
 
     print_r($_POST);
     $SQL =   "INSERT INTO heroes ( name, about_me, biography)
@@ -50,21 +42,9 @@ function createHero($name, $about_me, $biography)
 
 
 
-// Read
-function readHero()
-{
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Heroes";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+// READ
+function readHero(){
+   global $conn;
 
     $sql = "SELECT name, about_me, biography FROM heroes";
     $result = mysqli_query($conn, $sql);
@@ -80,21 +60,12 @@ function readHero()
 
     mysqli_close($conn);
 }
+
+// UPDATE
 function updateHero($id, $name, $about_me, $biography)
 {
-    //
-    //array_splice($_SESSION["heroes"],$index,1,[[$name, $tagline]]);
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Heroes";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+   
+    global $conn;
 
     $sql = "UPDATE heroes SET about_me='$about_me', name='$name', biography='$biography' WHERE heroes.id=$id";
 
@@ -106,21 +77,12 @@ function updateHero($id, $name, $about_me, $biography)
     $conn->close();
 }
 
+
+// DELETE
 function deleteHero($id)
 {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Heroes";
+    global $conn;
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-
-    // sql to delete a record
     $sql = "DELETE FROM heroes WHERE id='$id'";
 
     if ($conn->query($sql) === TRUE) {
@@ -129,20 +91,10 @@ function deleteHero($id)
         echo "Error deleting record: " . $conn->error;
     }
 }
-
+// READALL
 function heroData()
 {
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Heroes";
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    global $conn;
     $SQL = "SELECT 
             heroes.name, 
             heroes.about_me, 
@@ -168,27 +120,13 @@ function heroData()
 
     mysqli_close($conn);
 }
-
-
-// Create connection
-
-// Check connection
-
-
-
-
-
-
-
-//echo "<pre>" . print_r($action, 1) . "</pre>";
-
+// $WITCH
 if (isset($_GET['action'])) {
     $action = $_GET['action'];
     switch ($action) {
 
         case "create":
-            // conn
-            // check isset($_GET['name']);
+            
             if (isset($_GET["name"])) {
                 $name = $_GET["name"];
             }
@@ -198,15 +136,12 @@ if (isset($_GET['action'])) {
             if (isset($_GET["biography"])) {
                 $biography  = $_GET["biography"];
             }
-
-            //echo "hey create";
             createHero($name, $about_me, $biography);
             break;
         case "read":
             readHero();
             break;
         case "update":
-            // updateHero($id, $name, $tagline);
             updateHero($_POST["id"], $_POST["name"], $_POST["about_me"], $_POST["biography"]);
             break;
         case "delete":
@@ -217,6 +152,6 @@ if (isset($_GET['action'])) {
             break;
         default:
 
-            // init();
+            
     }
 }
