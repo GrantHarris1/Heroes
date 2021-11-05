447deed57dca14d2436fe97f847c05691bb51772
grantharriselectric <grantharriselectric@gmail.com>
2021-11-05 09:12:52 -0400

    working

--- myheroes.php
+++ myheroes.php
@@ -8,83 +8,80 @@
 * close connection -->
 <?php
 $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Heroes";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-//echo "Connected successfully";
-function createHero($name, $about_me, $biography){
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Heroes";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);}
-
-  print_r($_POST);
-    $SQL =   "INSERT INTO heroes ( name, about_me, biography)
-       VALUES ('$name', '$about_me', '$biography' )";
-       echo $SQL;
-    if ($conn->query($SQL) === TRUE) {
-            echo "New Hero created successfully";
-            echo '<br>';
-        }
-        else {
-            echo "Error: " . $SQL . "<br>" . $conn->error;
-        }
-        mysqli_close($conn);
-
-   
-
-}
-
-
-
-// Read
-function readHero(){
-$servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "Heroes";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//echo "Connected successfully";
+function createHero($name, $about_me, $biography)
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Heroes";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    print_r($_POST);
+    $SQL =   "INSERT INTO heroes ( name, about_me, biography)
+       VALUES ('$name', '$about_me', '$biography' )";
+    echo $SQL;
+    if ($conn->query($SQL) === TRUE) {
+        echo "New Hero created successfully";
+        echo '<br>';
+    } else {
+        echo "Error: " . $SQL . "<br>" . $conn->error;
+    }
+    mysqli_close($conn);
 }
 
-$sql = "SELECT name, about_me, biography FROM heroes";
-$result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "Name " . $row["name"]. "  " . $row["about_me"]. " " . $row["biography"]. "<br>";
-  }
-} else {
-  echo "0 results";
+
+// Read
+function readHero()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Heroes";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $sql = "SELECT name, about_me, biography FROM heroes";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "NAME: " . $row["name"] . "<br>" . "  ABOUT_ME: " . $row["about_me"] . "<br>" . " BIOGRAPHY:" . $row["biography"] . "<br> <br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    mysqli_close($conn);
 }
-
-mysqli_close($conn);
-
-   
-
-}
-function updateHero($id, $name, $about_me, $biography){
+function updateHero($id, $name, $about_me, $biography)
+{
     //
     //array_splice($_SESSION["heroes"],$index,1,[[$name, $tagline]]);
     $servername = "localhost";
@@ -98,19 +95,19 @@ function updateHero($id, $name, $about_me, $biography){
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    
+
     $sql = "UPDATE heroes SET about_me='$about_me', name='$name', biography='$biography' WHERE heroes.id=$id";
-    
+
     if ($conn->query($sql) === TRUE) {
         echo "Record updated successfully";
     } else {
         echo "Error updating record: " . $conn->error;
     }
     $conn->close();
-
 }
 
-function deleteHero($id){
+function deleteHero($id)
+{
     $servername = "localhost";
     $username = "root";
     $password = "";
@@ -120,18 +117,58 @@ function deleteHero($id){
     $conn = new mysqli($servername, $username, $password, $dbname);
     // Check connection
     if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
     }
 
     // sql to delete a record
     $sql = "DELETE FROM heroes WHERE id='$id'";
 
     if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
+        echo "Record deleted successfully";
     } else {
-    echo "Error deleting record: " . $conn->error;
+        echo "Error deleting record: " . $conn->error;
     }
 }
+
+function heroData()
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Heroes";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $SQL = "SELECT 
+            heroes.name, 
+            heroes.about_me, 
+            GROUP_CONCAT(ability_type.ability SEPARATOR ', ') AS powers
+            FROM heroes
+            INNER JOIN abilities ON abilities.hero_id = heroes.id
+            INNER JOIN ability_type ON ability_type.id = abilities.ability_id 
+            GROUP BY heroes.name";
+
+            // echo $SQL;
+
+    $result = mysqli_query($conn, $SQL);
+    // echo $result;
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "NAME: " . $row["name"] . "<br>" . "  ABOUT_ME: " . $row["about_me"] . "<br>" . "  POWERS: " . $row["powers"] . "<br>" . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    mysqli_close($conn);
+}
+
 
 // Create connection
 
@@ -152,17 +189,16 @@ if (isset($_GET['action'])) {
         case "create":
             // conn
             // check isset($_GET['name']);
-            if(isset($_GET["name"])){
+            if (isset($_GET["name"])) {
                 $name = $_GET["name"];
-
             }
-            if(isset($_GET["about_me"])){
+            if (isset($_GET["about_me"])) {
                 $about_me = $_GET["about_me"];
             }
-            if(isset($_GET["biography"])){
+            if (isset($_GET["biography"])) {
                 $biography  = $_GET["biography"];
             }
-            
+
             //echo "hey create";
             createHero($name, $about_me, $biography);
             break;
@@ -170,14 +206,17 @@ if (isset($_GET['action'])) {
             readHero();
             break;
         case "update":
-        // updateHero($id, $name, $tagline);
+            // updateHero($id, $name, $tagline);
             updateHero($_POST["id"], $_POST["name"], $_POST["about_me"], $_POST["biography"]);
             break;
         case "delete":
             deleteHero($_GET["id"]);
             break;
+        case "readAll":
+            heroData();
+            break;
         default:
-        
-           // init();
-        }
+
+            // init();
     }
+}
